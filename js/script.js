6021cb71f2b8a5efc79dab8a853656dc469a3e11
ribeiromatheus <ribeiro.matheuspereiraribeiro>
2019-02-28 16:12:26 -0300

    initial commit

---
+++ js/script.js
@@ -0,0 +1,25 @@
+var txaText = document.getElementById('txaText'),
+txaNewText = document.getElementById('txaNewText'),
+btnUpper = document.getElementById('btnUpper'),
+btnLower = document.getElementById('btnLower');
+btnUpperLower = document.getElementById('btnUpperLower');
+
+btnLower.addEventListener('click', toLower);
+btnUpper.addEventListener('click', toUpper);
+btnUpperLower.addEventListener('click', toUpperLower);
+
+function toLower() {
+    return displayText(txaText.value.toLowerCase());
+}
+
+function toUpper() {
+    return displayText(txaText.value.toUpperCase());
+}
+
+function toUpperLower() {
+    
+}
+
+function displayText(text) {
+    return txaNewText.value = text;
+}
