f0ca76a4479cd08f0d1b7831a4799a17a9f6c22a
ribeiromatheus <ribeiro.matheuspereiraribeiro>
2019-03-15 16:03:40 -0300

    Update 15/03/19

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 var txaText = document.getElementById('txaText'),
-txaNewText = document.getElementById('txaNewText'),
-btnUpper = document.getElementById('btnUpper'),
-btnLower = document.getElementById('btnLower');
-btnUpperLower = document.getElementById('btnUpperLower');
+    txaNewText = document.getElementById('txaNewText'),
+    btnUpper = document.getElementById('btnUpper'),
+    btnLower = document.getElementById('btnLower'),
+    btnUpperLower = document.getElementById('btnUpperLower');
 
 btnLower.addEventListener('click', toLower);
 btnUpper.addEventListener('click', toUpper);
@@ -17,7 +17,15 @@ function toUpper() {
 }
 
 function toUpperLower() {
-    
+    let text = txaText.value, i = 0, ans = "";
+
+    while (i < text.length) {
+        if (i % 2 == 0) ans += text[i].toUpperCase();
+        else ans += text[i].toLocaleLowerCase();
+        i++;
+    }
+
+    return displayText(ans);
 }
 
 function displayText(text) {
